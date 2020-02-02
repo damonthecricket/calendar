1904a456b0d219169ee04ec8bb088318ed450022
Damon Cricket <d.a.tsvirko@gmail.com>
2020-02-02 17:24:51 +0200

    First commit.

---
+++ Calendar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calendar
+//
+//  Created by Damon Cricket on 02.02.2020.
+//  Copyright © 2020 DC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
